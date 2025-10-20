ed54b81994f1f62f6f79849f42ad66de7896025c
TrungTuz2521 <trungtu648788@gmail.com>
2025-10-20 19:05:12 +0700

    update ThongBaovaNoiquy

--- obj/Debug/net8.0/KTX.AssemblyInfo.cs
+++ obj/Debug/net8.0/KTX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KTX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c78d0761767c184fe677e83a99079cbed9f216e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24374ea6df59d864314ce7266178e7a6c94da7c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("KTX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KTX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
